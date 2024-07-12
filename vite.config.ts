bf621237175083e4228b54af9197c88930ada9fd
Enzo Morata <enzo.morata@uc.cl>
2024-07-11 22:47:57 -0400

    style: apply 4 spaces indent

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    origin: 'http://localhost:5173'
-  }
+    plugins: [react()],
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: true,
+        origin: 'http://localhost:5173'
+    }
 })
